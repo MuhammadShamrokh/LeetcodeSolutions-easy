16083c7b206947b409ac7cd6e7c7e42196b13b8e
ShamrokhMuhammad <87933802+ShamrokhMuhammad@users.noreply.github.com>
2024-09-07 17:25:23 +0300

    Two Sum

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,4 +1,26 @@
 package shamrokh.muhammad.leetcode.solution;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // hashmap to store all values we have seen
+        Map<Integer,Integer> hashmap = new HashMap<Integer,Integer>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            // target - current value exist in hashmap, we found two sum
+            if(hashmap.containsKey(target - nums[i]))
+            {
+                return new int[]{hashmap.get(target - nums[i]), i};
+            }
+            else
+            {
+                hashmap.put(nums[i],i);
+            }
+        }
+
+        return new int[2];
+    }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -1,10 +1,50 @@
 package shamrokh.muhammad.leetcode.solution;
 
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SolutionTest {
+    Solution solution;
+    @BeforeEach
+    void setup(){
+        solution = new Solution();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test 1")
+
+    void TestOne(){
+        int[] expected = new int[]{0,1};
+
+        int [] actual = solution.twoSum(new int[]{2,7,11,15}, 9);
+
+        assertArrayEquals(expected, actual, "result should be [0,1]");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test 2")
+    void TestTwo(){
+        int[] expected = new int[]{1,2};
+
+        int [] actual = solution.twoSum(new int[]{3,2,4}, 6);
+
+        assertArrayEquals(expected, actual, "result should be [1,2]");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test 3")
+    void TestThree(){
+        int[] expected = new int[]{0,1};
+
+        int [] actual = solution.twoSum(new int[]{3,3}, 6);
+
+        assertArrayEquals(expected, actual, "result should be [0,1]");
+    }
 
 }
