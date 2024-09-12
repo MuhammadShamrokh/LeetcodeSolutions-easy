dfcda19f7e7daf7fdff93ff8ae9e8e6fda8fbe76
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-12 20:07:56 +0300

    908. Smallest Range I

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -5,22 +5,28 @@ import shamrokh.muhammad.leetcode.datastructure.TreeNode;
 import java.util.*;
 
 class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        int leftIndex = 0;
-        int rightIndex = nums.length - 1;
+    public int smallestRangeI(int[] nums, int k) {
+        int max = nums[0];
+        // highest number in array is 10000 as given in the question
+        int min = 10000;
 
-        while(leftIndex < rightIndex){
-            if(nums[leftIndex] %2 == 0) {
-                leftIndex++;
+
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i] > max){
+                max = nums[i];
             }
-            else {
-                int temp = nums[leftIndex];
-                nums[leftIndex] = nums[rightIndex];
-                nums[rightIndex] = temp;
-                rightIndex--;
+
+            if(nums[i] < min){
+                min = nums[i];
             }
         }
 
-        return nums;
+        // we add the k to the minimum number and subtract k to the maximum number
+        // to receive the minimum possible result
+        min += k;
+        max -= k;
+
+        // if the result is lower than 0, that mean we subtract less than k from the max value inorder to get lower value (which is zero)
+        return Math.max(0, max - min);
     }
 }
