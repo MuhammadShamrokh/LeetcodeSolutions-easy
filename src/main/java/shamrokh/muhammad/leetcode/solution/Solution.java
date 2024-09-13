08f7dfe6a766ba01c8ca2b08daa7b5547a783829
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-13 21:49:20 +0300

    938. Range Sum of BST

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -5,5 +5,21 @@ import shamrokh.muhammad.leetcode.datastructure.TreeNode;
 import java.util.*;
 
 class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)
+            return 0;
 
+
+        // root value in the range
+        if(root.val >= low && root.val <= high){
+            return rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high) + root.val;
+        }
+
+        //root value smaller than the range
+        if(root.val < low)
+            return rangeSumBST(root.right, low, high);
+
+        //root value bigger than the range
+        return rangeSumBST(root.left, low, high);
+    }
 }
