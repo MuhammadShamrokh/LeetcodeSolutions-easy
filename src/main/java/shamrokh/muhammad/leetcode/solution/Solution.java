bf0b1fee92ac0eaf115c6d4f0dd80a026032072a
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-14 19:52:21 +0300

    942. DI String Match

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,45 +1,19 @@
 package shamrokh.muhammad.leetcode.solution;
 
-import shamrokh.muhammad.leetcode.datastructure.ListNode;
-import shamrokh.muhammad.leetcode.datastructure.TreeNode;
-import java.util.*;
-
 class Solution {
-    public boolean validMountainArray(int[] arr) {
-        // if length < 3 then cant be a mountain array
-        if(arr.length < 3)
-            return false;
-
-        //edge case: check if array start with increasing values
-        if(arr[0] >= arr[1])
-            return false;
-
-        int index = 1;
-
-        // iterating over increasing values area
-        while(index < arr.length && arr[index] >= arr[index-1]){
-            // mountain array cant have equal values
-            if(arr[index] == arr[index-1])
-                return false;
-
-            index++;
-        }
-
-        // checking if array has decreasing values area
-        if(index == arr.length)
-            return false;
-
-        // iterating over decreasing values area
-        while(index < arr.length && arr[index] <= arr[index-1]){
-            // mountain array cant have equal values
-            if(arr[index] == arr[index-1])
-                return false;
-
-            index ++;
-        }
+    public int[] diStringMatch(String s) {
+        int length = s.length();
+        int lowValues = 0;
+        int highValues = length;
+        int[] solution = new int[length + 1];
 
 
-        return index == arr.length;
+        for(int i=0;i<length;i++)
+            solution[i] = s.charAt(i) == 'D'? highValues--: lowValues++;
+
+        // Append the last remaining number (low or high, both are equal at this point)
+        // We can also use high since low == high
+        solution[length] = lowValues;
+        return solution;
     }
-
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -1,12 +1,6 @@
 package shamrokh.muhammad.leetcode.solution;
 
 import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,28 +16,28 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertFalse(solution.validMountainArray(new int[]{2,1}));
+
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertFalse(solution.validMountainArray(new int[]{3,3,5}));
+
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertTrue(solution.validMountainArray(new int[]{0,3,2,1}));
+
     }
 
     @Test
     @Order(4)
     @DisplayName("Test 4")
     void TestFour(){
-        assertTrue(solution.validMountainArray(new int[]{1,2,3,4,5,4,3,2,1}));
+
     }
 
 }
