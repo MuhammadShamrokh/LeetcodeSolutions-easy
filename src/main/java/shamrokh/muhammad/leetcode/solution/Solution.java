8e4090225aafd668005c37483e4c3c6de412433d
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-20 15:55:05 +0300

    1018. Binary Prefix Divisible By 5

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,46 +1,23 @@
 package shamrokh.muhammad.leetcode.solution;
 
 
-class Solution {
-    public boolean canThreePartsEqualSum(int[] arr) {
-        int arraySum = sumOfArray(arr);
+import java.util.ArrayList;
+import java.util.List;
 
-        // if array sum is not divisible by 3, then we cant partition
-        if(arraySum%3 != 0){
-            return false;
+public class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int remainder = 0; // To track the remainder of the number mod 5
+
+        // Iterate through each bit in the binary array
+        for (int num : nums) {
+            // Update the number (remainder) by shifting it left and adding the current bit
+            remainder = (remainder * 2 + num) % 5;
+
+            // If the remainder is 0, the number is divisible by 5
+            result.add(remainder == 0);
         }
 
-        int partitionSum = arraySum/3;
-        int partitionsCount = 0;
-        arraySum = 0;
-
-        for(int i=0;i<arr.length;i++){
-            arraySum += arr[i];
-
-            // we found a partition
-            if(arraySum == partitionSum){
-                partitionsCount += 1;
-                arraySum = 0;
-            }
-
-            // If we find 2 valid partitions before the end of the array,
-            // we return true, because the third part will automatically work
-            if(partitionsCount == 2 && i<arr.length-1)
-                return true;
-        }
-
-        // we did not find 2 partitions before end of array
-        // we return false because we cant split array into 3 equal parts
-        return false;
-    }
-
-    private int sumOfArray(int[] arr){
-        int sum=0;
-
-        for(int elem:arr){
-            sum+=elem;
-        }
-
-        return sum;
+        return result;
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -2,6 +2,7 @@ package shamrokh.muhammad.leetcode.solution;
 
 import org.junit.jupiter.api.*;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -16,21 +17,21 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertTrue(solution.canThreePartsEqualSum(new int[]{0,2,1,-6,6,-7,9,1,2,0,1}));
+
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo() {
-        assertFalse(solution.canThreePartsEqualSum(new int[]{0,2,1,-6,6,7,9,-1,2,0,1}));
+
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertTrue(solution.canThreePartsEqualSum(new int[]{3,3,6,5,-2,2,5,1,-9,4}));
+
     }
 
     @Test
