e9081c8032de63dcf46b2e2ea05574565e9ccecb
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-25 20:33:56 +0300

    1122. Relative Sort Array

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,21 +1,47 @@
 package shamrokh.muhammad.leetcode.solution;
 
-
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Solution {
-    public double trimMean(int[] arr) {
-        int length = arr.length;
-        int fivePercentOfElements = (int)(length*0.05);
-        int sum=0;
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer,Integer> arr1ElementsCount = new HashMap<>();
+        Set<Integer> arr1ElementsSet = new TreeSet<>();
+        Set<Integer> arr2ElementsSet = new TreeSet<>();
+        int[] result = new int[arr1.length];
+        int index = 0;
 
-        // sorting array
-        Arrays.sort(arr);
-        // iterating over array to calculate mean
-        for(int i=fivePercentOfElements;i<length-fivePercentOfElements;i++)
-            sum+=arr[i];
+        // counting elements appearances in arr1 and storing into hashmap and set
+        for(int elem: arr1){
+            arr1ElementsCount.put(elem, arr1ElementsCount.getOrDefault(elem,0)+1);
+            arr1ElementsSet.add(elem);
+        }
 
-        return (double) sum /(length - fivePercentOfElements*2);
 
+        for(int elem:arr2){
+            // storing arr2 elements into set
+            arr2ElementsSet.add(elem);
+            // building result array
+            for(int i=0;i<arr1ElementsCount.get(elem);i++){
+                result[index] = elem;
+                index++;
+            }
+        }
+
+        // finding all the elements in arr2 and not in arr1
+        arr1ElementsSet.removeAll(arr2ElementsSet);
+
+        // filling result with elements that did not appear in arr2
+        // using TreeSet, foreach will be scanning elements in sorted order
+        for(int elem:arr1ElementsSet){
+            for(int i=0;i<arr1ElementsCount.get(elem);i++) {
+                result[index] = elem;
+                index++;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -17,25 +17,30 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        double expected = 2.00000;
+        int[] expected = new int[]{2,2,2,1,4,3,3,9,6,7,19};
 
-        assertEquals(expected, solution.trimMean(new int[]{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3}));
+        assertArrayEquals(expected, solution.relativeSortArray(new int[]{2,3,1,3,2,4,6,7,9,2,19},
+                                                                new int[]{2,1,4,3,9,6}));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo() {
-        double expected = 4.00000;
+        int[] expected = new int[]{22,28,8,6,17,44};
 
-        assertEquals(expected, solution.trimMean(new int[]{6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0}));
+        assertArrayEquals(expected, solution.relativeSortArray(new int[]{28,6,22,8,44,17},
+                                                                new int[]{22,28,8,6}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
+        int[] expected = new int[]{2,42,38,0,43,21,5,7,12,12,13,23,24,24,27,29,30,31,33,48};
 
+        assertArrayEquals(expected, solution.relativeSortArray(new int[]{2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31},
+                new int[]{2,42,38,0,43,21}));
     }
 
     @Test
@@ -46,3 +51,4 @@ class SolutionTest {
     }
 
 }
+
