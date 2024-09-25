d6e6006ab17e869ff6db042b37614d51e0739919
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-25 21:16:10 +0300

    1287. Element Appearing More Than 25% In Sorted Array

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,47 +1,29 @@
 package shamrokh.muhammad.leetcode.solution;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-
 class Solution {
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer,Integer> arr1ElementsCount = new HashMap<>();
-        Set<Integer> arr1ElementsSet = new TreeSet<>();
-        Set<Integer> arr2ElementsSet = new TreeSet<>();
-        int[] result = new int[arr1.length];
-        int index = 0;
+    public int findSpecialInteger(int[] arr) {
+        int length = arr.length;
+        double fiftyTwoPercent = length * 0.25;
+        int elementCounter = 0;
+        int currentElement = arr[0];
 
-        // counting elements appearances in arr1 and storing into hashmap and set
-        for(int elem: arr1){
-            arr1ElementsCount.put(elem, arr1ElementsCount.getOrDefault(elem,0)+1);
-            arr1ElementsSet.add(elem);
-        }
+        // iterating over elements to find element with requirement appearances amount
+        for (int elem : arr) {
+            if (elem == currentElement) {
+                elementCounter++;
+            } else {
+                // new element, checking if the prev element answers the appearances requirements
+                if (elementCounter > fiftyTwoPercent)
+                    return currentElement;
 
-
-        for(int elem:arr2){
-            // storing arr2 elements into set
-            arr2ElementsSet.add(elem);
-            // building result array
-            for(int i=0;i<arr1ElementsCount.get(elem);i++){
-                result[index] = elem;
-                index++;
+                // the previous element did not appear more than 25% of the array length
+                currentElement = elem;
+                elementCounter = 1;
             }
         }
 
-        // finding all the elements in arr2 and not in arr1
-        arr1ElementsSet.removeAll(arr2ElementsSet);
-
-        // filling result with elements that did not appear in arr2
-        // using TreeSet, foreach will be scanning elements in sorted order
-        for(int elem:arr1ElementsSet){
-            for(int i=0;i<arr1ElementsCount.get(elem);i++) {
-                result[index] = elem;
-                index++;
-            }
-        }
-
-        return result;
+        // the element did not appear in the middle of the array
+        // the wanted element must be the last element
+        return arr[length-1];
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -17,30 +17,21 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        int[] expected = new int[]{2,2,2,1,4,3,3,9,6,7,19};
-
-        assertArrayEquals(expected, solution.relativeSortArray(new int[]{2,3,1,3,2,4,6,7,9,2,19},
-                                                                new int[]{2,1,4,3,9,6}));
+        assertEquals(6, solution.findSpecialInteger(new int[]{1,2,2,6,6,6,6,7,10}));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo() {
-        int[] expected = new int[]{22,28,8,6,17,44};
-
-        assertArrayEquals(expected, solution.relativeSortArray(new int[]{28,6,22,8,44,17},
-                                                                new int[]{22,28,8,6}));
+        assertEquals(1, solution.findSpecialInteger(new int[]{1, 1}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        int[] expected = new int[]{2,42,38,0,43,21,5,7,12,12,13,23,24,24,27,29,30,31,33,48};
 
-        assertArrayEquals(expected, solution.relativeSortArray(new int[]{2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31},
-                new int[]{2,42,38,0,43,21}));
     }
 
     @Test
