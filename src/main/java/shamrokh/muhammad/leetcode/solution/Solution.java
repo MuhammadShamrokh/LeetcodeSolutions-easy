112f4f541518b407b2a7601d4042a8c5bbb1d64a
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-28 11:58:55 +0300

    1184. Distance Between Bus Stops

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,41 +1,30 @@
 package shamrokh.muhammad.leetcode.solution;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution{
-    private static final Map<String,String> months = new HashMap<>(){{
-        put("Jan","01");
-        put("Feb","02");
-        put("Mar","03");
-        put("Apr","04");
-        put("May","05");
-        put("Jun","06");
-        put("Jul","07");
-        put("Aug","08");
-        put("Sep","09");
-        put("Oct","10");
-        put("Nov","11");
-        put("Dec","12");
-    }};
-
-
-    public String reformatDate(String date) {
-        String[] splitDate = date.split(" ");
-        String day = convertDayFormat(splitDate[0]);
-        String month = months.get(splitDate[1]);
-        String year = splitDate[2];
-
-        return String.format("%s-%s-%s", year, month, day);
-
-    }
-
-    private String convertDayFormat(String day){
-        if(Character.isAlphabetic(day.charAt(1))){
-            return "0"+day.charAt(0);
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        // if the start and end points are equal, then the total distance is 0;
+        if(start == destination){
+            return 0;
         }
-        else{
-            return day.substring(0, 2);
+
+        int startToEndLength = 0;
+        int endToStartLength = 0;
+        int distanceArrayLength = distance.length;
+        int scanner = start;
+
+        // calculating the distance clockwise
+        while(scanner != destination){
+            startToEndLength += distance[scanner];
+            scanner = (scanner+1)%distanceArrayLength;
         }
+
+        // calculating the distance counterclockwise
+        while(scanner != start){
+            endToStartLength += distance[scanner];
+            scanner = (scanner+1)%distanceArrayLength;
+        }
+
+        // returning the min length
+        return Math.min(startToEndLength, endToStartLength);
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -17,21 +17,22 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertEquals("2052-10-20", solution.reformatDate("20th Oct 2052"));
+        assertEquals(1, solution.distanceBetweenBusStops(new int[]{1,2,3,4},0,1));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertEquals("1933-06-06", solution.reformatDate("6th Jun 1933"));
+        assertEquals(3, solution.distanceBetweenBusStops(new int[]{1,2,3,4},0,2));
+
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertEquals("1960-05-26", solution.reformatDate("26th May 1960"));
+        assertEquals(4, solution.distanceBetweenBusStops(new int[]{1,2,3,4},0,3));
     }
 
     @Test
