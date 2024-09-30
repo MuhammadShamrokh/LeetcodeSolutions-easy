261c8118b16afff340dbacc245bd6ccd0cd4e640
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-30 14:42:47 +0300

    clean up

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,37 +1,8 @@
 package shamrokh.muhammad.leetcode.solution;
 
-import shamrokh.muhammad.leetcode.datastructure.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        //edge case: string of size 0 or 1
-        if(s.isEmpty() || s.length() == 1)
-            return s.length();
 
-        //implementing sliding window to scan the string
-        int leftIndex = 0;
-        int rightIndex = 0;
-        int longestSubstringLength = 0;
-        Map<Character, Integer> lastSeenIndexMap = new HashMap<>();
-
-        while(rightIndex < s.length()){
-            //  next character in sliding window exists in lastSeenIndexMap
-            if(lastSeenIndexMap.containsKey(s.charAt(rightIndex))){
-                // we update leftIndex if its value is smaller than the last seen current character index
-                if(leftIndex < lastSeenIndexMap.get(s.charAt(rightIndex)) +1)
-                    leftIndex = lastSeenIndexMap.get(s.charAt(rightIndex)) +1;
-            }
-
-            // update hashmap and sliding window
-            lastSeenIndexMap.put(s.charAt(rightIndex), rightIndex);
-            rightIndex++;
-            longestSubstringLength = Math.max(longestSubstringLength, rightIndex-leftIndex);
-        }
-
-        return longestSubstringLength;
-    }
 }
 
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -2,8 +2,6 @@ package shamrokh.muhammad.leetcode.solution;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SolutionTest {
@@ -17,21 +15,18 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertTrue(solution.uniqueOccurrences(new int[]{1,2,2,1,1,3}));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertFalse(solution.uniqueOccurrences(new int[]{1,2}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertTrue(solution.uniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
     }
 
     @Test
