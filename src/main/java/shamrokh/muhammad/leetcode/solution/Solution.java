1e25f8a8f5e7cbc95deaeeead6e62d944d33746b
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-09-30 14:56:40 +0300

    1217. Minimum Cost to Move Chips to The Same Position

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,8 +1,21 @@
 package shamrokh.muhammad.leetcode.solution;
 
-
-
 class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int oddCount = 0;
+        int evenCount = 0;
 
+        // Count chips at odd and even positions
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        // The minimum cost will be the smaller count
+        return Math.min(oddCount, evenCount);
+    }
 }
 
