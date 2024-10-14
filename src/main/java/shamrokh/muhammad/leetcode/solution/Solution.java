2af7da104f31be8e740332c4ccd0fe2dca42667c
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-10-14 19:09:46 +0300

    1304. Find N Unique Integers Sum up to Zero

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,23 +1,23 @@
 package shamrokh.muhammad.leetcode.solution;
 
 class Solution {
-    public int findNumbers(int[] nums) {
-        int resultCounter = 0;
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int index = 0;
 
-        //iterating over numbers to count numbers with even number of digits
-        for(int num:nums){
-            int digitsCount = 0;
-
-            // counting the number of digits in num
-            while(num > 0){
-                num = num/10;
-                digitsCount++;
-            }
-
-            // counting the word if its number of digits was even
-            resultCounter += digitsCount%2==0? 1:0;
+        if(n%2==1){
+            result[index] = 0;
+            index++;
+            n--;
         }
 
-        return resultCounter;
+        while(n > 0){
+            result[index++] = n/2;
+            result[index++] = -1*(n/2);
+
+            n -= 2;
+        }
+
+        return result;
     }
 }
