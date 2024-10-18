8e720b3dfd1c1fbff7785493e5663ca177120141
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-10-18 20:54:52 +0300

    1356. Sort Integers by The Number of 1 Bits

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,22 +1,89 @@
 package shamrokh.muhammad.leetcode.solution;
 
+import java.util.Comparator;
+
 class Solution {
-    public int removePalindromeSub(String s) {
-        return isPalindrome(s)? 1:2;
+    private static final Comparator<Integer> comparator;
+
+    static {
+        comparator = (o1, o2) -> {
+            int tmp1 = o1;
+            int tmp2 = o2;
+            int counter1 = 0;
+            int counter2 = 0;
+
+            while(tmp1>0){
+                if(tmp1%2==1){
+                    counter1++;
+                }
+
+                tmp1>>>=1;
+            }
+
+            while(tmp2>0){
+                if(tmp2%2==1){
+                    counter2++;
+                }
+
+                tmp2>>=1;
+            }
+
+            if(counter1 != counter2)
+                return counter1 - counter2;
+
+            return o1-o2;
+        };
     }
 
-    private boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
+    public int[] sortByBits(int[] arr) {
+        quickSort(arr, 0, arr.length-1);
 
-        while(start<end){
-            if(s.charAt(start) != s.charAt(end))
-                return false;
+        return arr;
+    }
 
-            start++;
-            end--;
+    private int partition(int[] arr, int low, int high) {
+
+        // Choose the pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and indicates
+        // the right position of pivot found so far
+        int i = low - 1;
+
+        // Traverse arr[low..high] and move all smaller
+        // elements to the left side. Elements from low to
+        // i are smaller after every iteration
+        for (int j = low; j <= high - 1; j++) {
+            if (comparator.compare(pivot,arr[j])>0) {
+                i++;
+                swap(arr, i, j);
+            }
         }
 
-        return true;
+        // Move pivot after smaller elements and
+        // return its position
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // Swap function
+    private  void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // The QuickSort function implementation
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            // pi is the partition return index of pivot
+            int pi = partition(arr, low, high);
+
+            // Recursion calls for smaller elements
+            // and greater or equals elements
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -18,22 +18,25 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertEquals(1, solution.removePalindromeSub("ababa"));
+        int[] expected = new int[]{0,1,2,4,8,3,5,6,7};
+
+        assertArrayEquals(expected, solution.sortByBits(new int[]{0,1,2,3,4,5,6,7,8}));
+
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertEquals(2, solution.removePalindromeSub("abb"));
+        int[] expected = new int[]{1,2,4,8,16,32,64,128,256,512,1024};
 
+        assertArrayEquals(expected, solution.sortByBits(new int[]{1024,512,256,128,64,32,16,8,4,2,1}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertEquals(2, solution.removePalindromeSub("baabb"));
 
     }
 
@@ -41,7 +44,6 @@ class SolutionTest {
     @Order(4)
     @DisplayName("Test 4")
     void TestFour(){
-        assertEquals(1, solution.removePalindromeSub("a"));
     }
 
 }
