70e31b6cd90131b4fe7880004f92ca2f4b6a4753
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-10-21 21:34:02 +0300

    1374. Generate a String With Characters That Have Odd Counts

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,25 +1,14 @@
 package shamrokh.muhammad.leetcode.solution;
 
 class Solution {
-    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-        boolean biggerDistanceThanInput;
-        int resultCounter = 0;
-
-        for (int num1 : arr1) {
-            biggerDistanceThanInput = true;
-
-            for (int num2 : arr2) {
-                if (Math.abs(num1 - num2) <= d) {
-                    biggerDistanceThanInput = false;
-                    break;
-                }
-            }
-
-            if (biggerDistanceThanInput)
-                resultCounter++;
+    public String generateTheString(int n) {
+        // n is even, we return ab..b (odd appearances of b)
+        if(n%2==0){
+            return "a"+"b".repeat(n-1);
         }
 
-        return resultCounter;
+        // we return a..a (odd appearances of a)
+        return "a".repeat(n);
     }
 }
 
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -18,21 +18,18 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertEquals(2, solution.findTheDistanceValue(new int[]{4,5,8},new int[]{10,9,1,8}, 2));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertEquals(2, solution.findTheDistanceValue(new int[]{1,4,2,3}, new int[]{-4,-3,6,10,20,30},3));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
-        assertEquals(1, solution.findTheDistanceValue(new int[]{2,1,100,3},new int[]{-5,-2,10,-3,7},6));
     }
 
     @Test
