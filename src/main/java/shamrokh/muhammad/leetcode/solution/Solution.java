2a110d4a7afb824855db4b8aa4fad1116a180657
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-10-23 20:31:22 +0300

    1389. Create Target Array in the Given Order

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -1,44 +1,29 @@
 package shamrokh.muhammad.leetcode.solution;
 
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
-    private final int MAX_FOUR_DIGITS_SUM = 37;
-    public int countLargestGroup(int n) {
-        int[] groupsSize = new int[MAX_FOUR_DIGITS_SUM];
-        int maxGroupSize = 0;
-        int groupsWithMaxSize = 0;
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> result = new LinkedList<>();
+        int[] resultArray = new int[nums.length];
+        int copyCounter = 0;
 
-        // iterating over the numbers [1, n]
-        for(int i=1;i<=n;i++){
-            // sum the digits
-            int digitsSum = sumNumDigits(i);
-
-            // add result to corresponding group
-            groupsSize[digitsSum]++;
-        }
-
-        // finding the number of groups with max size
-        for(int size:groupsSize){
-            if(size > maxGroupSize){
-                maxGroupSize = size;
-                groupsWithMaxSize = 1;
+        for(int i=0;i< nums.length;i++){
+            if(result.size() <= index[i]){
+                result.add(nums[i]);
             }
-            else if(size == maxGroupSize){
-                groupsWithMaxSize++;
+            else{
+                result.add(index[i], nums[i]);
             }
         }
 
-        return groupsWithMaxSize;
-    }
-
-    private int sumNumDigits(int num) {
-        int digitsSum = 0;
-
-        while(num > 0){
-            digitsSum += (num%10);
-            num /=10;
+        for(int num:result){
+            resultArray[copyCounter] = num;
+            copyCounter++;
         }
 
-        return digitsSum;
+        return resultArray;
     }
 }
 
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -18,20 +18,27 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        assertEquals(4, solution.countLargestGroup(13));
+        int[] expected = new int[]{0,4,1,3,2};
+
+        assertArrayEquals(expected, solution.createTargetArray(new int[]{0,1,2,3,4}, new int[]{0,1,2,2,1}));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        assertEquals(2, solution.countLargestGroup(2));
+        int[] expected = new int[]{0,1,2,3,4};
+
+        assertArrayEquals(expected, solution.createTargetArray(new int[]{1,2,3,4,0}, new int[]{0,1,2,3,0}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
+        int[] expected = new int[]{1};
+
+        assertArrayEquals(expected, solution.createTargetArray(new int[]{1}, new int[]{0}));
     }
 
     @Test
