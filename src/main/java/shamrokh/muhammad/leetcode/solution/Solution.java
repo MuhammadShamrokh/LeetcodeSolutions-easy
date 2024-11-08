db83b5718619bc4f3f2c10a168889252f54c98d8
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-11-08 20:04:58 +0200

    1480. Running Sum of 1d Array

--- src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solution/Solution.java
@@ -2,44 +2,14 @@ package shamrokh.muhammad.leetcode.solution;
 
 
 class Solution {
-    public double average(int[] salary) {
-        int length = salary.length;
-        int salarySum = sumArray(salary);
-        int maxValue = findMaxValue(salary);
-        int minValue = findMinValue(salary);
+    public int[] runningSum(int[] nums) {
+        int[] runningSumArray = new int[nums.length];
 
-        return (double)(salarySum - (maxValue+minValue)) / (length-2);
-    }
-
-    private int findMaxValue(int[] salary){
-        int maxValue = salary[0];
-
-        for(int i=1;i< salary.length;i++){
-            if(salary[i]>maxValue)
-                maxValue = salary[i];
+        runningSumArray[0] = nums[0];
+        for(int i=1;i< nums.length;i++){
+            runningSumArray[i] = runningSumArray[i-1] + nums[i];
         }
 
-        return maxValue;
-    }
-
-    private int findMinValue(int[] salary){
-        int minValue = salary[0];
-
-        for(int i=1;i< salary.length;i++){
-            if(salary[i]<minValue)
-                minValue = salary[i];
-        }
-
-        return minValue;
-    }
-
-    private int sumArray(int[] salary){
-        int sum=0;
-
-        for(int elem:salary){
-            sum+=elem;
-        }
-
-        return sum;
+        return runningSumArray;
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solution/SolutionTest.java
@@ -18,25 +18,33 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test 1")
     void TestOne(){
-        //Input: salary = [4000,3000,1000,2000]
-        //Output: 2500.00000
-        assertEquals(2500, solution.average(new int[]{4000,3000,1000,2000}));
+        //Input: nums = [1,2,3,4]
+        //Output: [1,3,6,10]
+        int[] expected = new int[]{1,3,6,10};
+
+        assertArrayEquals(expected, solution.runningSum(new int[]{1,2,3,4}));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test 2")
     void TestTwo(){
-        //Input: salary = [1000,2000,3000]
-        //Output: 2000.00000
-        assertEquals(2000, solution.average(new int[]{1000,2000,3000}));
+        //Input: nums = [1,1,1,1,1]
+        //Output: [1,2,3,4,5]
+        int[] expected = new int[]{1,2,3,4,5};
+
+        assertArrayEquals(expected, solution.runningSum(new int[]{1,1,1,1,1}));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test 3")
     void TestThree(){
+        //Input: nums = [3,1,2,10,1]
+        //Output: [3,4,6,16,17]
+        int[] expected = new int[]{3,4,6,16,17};
 
+        assertArrayEquals(expected, solution.runningSum(new int[]{3,1,2,10,1}));
     }
 
     @Test
